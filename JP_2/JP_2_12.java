24d99630143f5858f1d9eaf6e8af330f18e0b025
Antonio_Work <cve-anton@ya.ru>
2015-03-16 17:49:39 +0300

    add JP_2_11.java

--- JP_2/JP_2_12.java
+++ JP_2/JP_2_12.java
@@ -3,5 +3,27 @@ package JP_2;
 /**
  * Created by Antonio on 09.03.2015.
  */
+
+//: JP_2/JP_2_12.java
+import java.util.*;
+
+/** Первая программа-пример книги.
+ * выводит строку и текущее число.
+ * @author Брюс Эккель
+ * @author www.MindView.net
+ * @version 4.0
+ */
+
 public class JP_2_12 {
-}
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions No exceptions thrown
+     */
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня:");
+        System.out.println(new Date());
+    }
+} /* Output: (55% match)
+Привет, сегодня:
+Wed Oct 05 14:39:36 MDT 2005
+*///:~
