7b41ee362a7d5fa4ce8a80d931b40d7f1068bc90
Antonio_Work <cve-anton@ya.ru>
2015-03-17 15:44:45 +0300

    Add MyLib module with external java classes.

--- JP_3/JP_3_1.java
+++ JP_3/JP_3_1.java
@@ -3,5 +3,20 @@ package JP_3;
 /**
  * Created by atsvetkov on 16.03.2015.
  */
+
+//: JP_3/JP_3_1.java
+
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+
 public class JP_3_1 {
+    public static void main(String[] args) {
+        print("Привет, сегодня:");
+        print(new Date());
+
+        System.out.println("Привет, сегодня:");
+        System.out.println(new Date());
+    }
 }
+
